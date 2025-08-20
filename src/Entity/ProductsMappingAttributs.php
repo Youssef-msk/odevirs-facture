1e7761c8a85fd90ca45d2d16a84ff7dd2cffdf98
Youssef Meskine <meskineyoussef13@gmail.com>
2025-08-20 23:10:26 +0100

    correction

---
+++ src/Entity/ProductsMappingAttributs.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductsMappingAttributsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductsMappingAttributsRepository::class)]
+class ProductsMappingAttributs
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 5000)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 500, nullable: true)]
+    private ?string $picture = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): static
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+}
